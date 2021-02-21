80dc030b53988a7ebee44746082e9b2b7434f1c5
Luis <luisarmando1234@gmail.com>
2021-02-21 16:41:06 -0600

    :hammer: Adding Client Factory
    Added Manual ClientFactory

--- src/database/factories/ClientFactory.php
+++ src/database/factories/ClientFactory.php
@@ -1,32 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Inquid\YiiPassport\database\factories;
 
-use yii\db\ActiveQuery;
-use yii\db\ActiveRecord;
+use Faker\Factory;
 use Inquid\YiiPassport\models\OauthClients;
+use yii\base\Exception;
+use yii\base\Security;
 
 /**
  *
  */
 class ClientFactory
 {
-
-
-
     /**
      *
+     * @param array $data
+     * @return OauthClients
+     * @throws Exception
      */
-    public function generate(array $data): array {
+    public function generate(array $data): OauthClients {
+        // use the factory to create a Faker\Generator instance
+        $faker = Factory::create();
+
         $client = new OauthClients();
         $client->load([
-            'user_id' => null,
-            'name' => $this->faker->company,
-            'secret' => Str::random(40),
-            'redirect' => $this->faker->url,
-            'personal_access_client' => false,
-            'password_client' => false,
-            'revoked' => false,
+            'user_id' => $data['user_id'],
+            'name' => $data['name'] ?? $faker->company,
+            'secret' => (new Security())->generateRandomString(40),
+            'redirect' => $data['url'] ?? $faker->url,
+            'personal_access_client' => $data['personal_access_client'] ?? false,
+            'password_client' => $data['password_client'] ?? false,
+            'revoked' => $data['revoked'] ?? false,
         ]);
 
         return $client;
